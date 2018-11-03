5e7c49cd97b69a062c334d5a07ed22f5783b1d9d
Jarodimus <jrodabba@gmail.com>
2018-11-03 19:34:32 -0400

    Modified default route to go to BoardGame controller.  Fixed navbar link.

--- src/BoardGameGallery/App_Start/RouteConfig.cs
+++ src/BoardGameGallery/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace BoardGameGallery
@@ -11,12 +7,14 @@ namespace BoardGameGallery
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            //files with extension .axd are ignored by the routing engine
+            // requests for axd files are handled by ASP.net and shouldn't be routed to any of the websites controllers
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "BoardGame", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
