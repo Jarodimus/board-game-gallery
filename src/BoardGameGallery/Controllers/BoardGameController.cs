93bbc81f88bca4cf64741368a8aa4b389909775d
Jarodimus <jrodabba@gmail.com>
2018-11-03 15:02:53 -0400

    Changed board game controller to reference repository

--- src/BoardGameGallery/Controllers/BoardGameController.cs
+++ src/BoardGameGallery/Controllers/BoardGameController.cs
@@ -1,35 +1,20 @@
-﻿using BoardGameGallery.Models;
+﻿using BoardGameGallery.Data;
 using System.Web.Mvc;
 
 namespace BoardGameGallery.Controllers
 {
     public class BoardGameController : Controller
     {
-        public ActionResult Detail()
+        private BoardGameRepository _boardGameRepository = null;
+
+        public BoardGameController()
         {
-            Manufacturer ffg = new Manufacturer()
-            {
-                Name = "Fantasy Flight Games",
-                Abbrev = "ffg"
-            };
-            var boardGame = new BoardGame()
-            {
-                Id = 1,
-                Title = "Arkham Horror 3rd Edition",
-                ProductNumber = "#FFGAHB01",
-                DescriptionHtml = "<p>Massachusetts. 1926. Too long has the city of Arkham lived on its placid island of ignorance in the midst of the black seas of infinity. Only a small handful of unlucky investigators dare plunge into that abyss to struggle against the ancient evils that threaten reality from every corner of this New England town.</p><p>Arkham Horror is a cooperative game of mystery and terror for up to six players.Inspired by the writings of H.P.Lovecraft, each unique scenario puts you in the shoes of one of Arkham's investigators as they explore the streets of the city and work together to save humanity from unknowable horrors.</p>",
-                Contents = new string[]{
-                "5 Map Tiles",
-                "7 Streets",
-                "12 Investigators",
-                "4 Scenarios",
-                "469 Cards",
-                "1 Deck Holder",
-                "6 Dice",
-                "265 Tokens" },
-                NewArrival = true,
-                Manufacturer = ffg
-            };
+            _boardGameRepository = new BoardGameRepository();
+        }
+
+        public ActionResult Detail(int id)
+        {
+            var boardGame = _boardGameRepository.GetBoardGame(id);
             return View(boardGame);
         }
     }
